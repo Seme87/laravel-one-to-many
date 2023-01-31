fd7198780659cf3a1edb3feec025c13d2b481809
Seme87 <massimomonteleone@hotmail.com>
2023-01-31 15:58:23 +0100

    Aggiunge store all entita types

---
+++ resources/views/admin/Types/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+
+@section('content')
+
+   
+    
+    <div class="container">
+        <h1>Crea una nuova Tipologia</h1>
+        
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <div class="mt-4">
+           <form action="{{ route('admin.types.store') }}" method="POST" >
+            @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Inserisci il nome" value="{{ old('name') }}">
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Crea</button>
+            </form>
+        </div>
+    </div>
+  
+
+@endsection
